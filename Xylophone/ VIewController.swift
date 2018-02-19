4a8280d26d4e88cacb45369a918ca0a1633e5a4a
Pavan Sabnis <pvnsbns@gmail.com>
2018-02-19 15:18:33 -0800

    Edited bundle identifier name and implemented code for sound1 being played when any button is pressed.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -2,13 +2,18 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
+
 //
 
 import UIKit
+//import library in order to use AVAudioPlayer
+import AVFoundation
+
 
 class ViewController: UIViewController{
+    // Create global instance for audio player
+    
+    var player: AVAudioPlayer?
     
 
     override func viewDidLoad() {
@@ -18,12 +23,35 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        //Although the music buttons have the same IBAction, the tag property differentiates each button
+        var soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")
         
-        
-        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url!)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+            
     }
+        
+}
     
   
 
-}
+
 
