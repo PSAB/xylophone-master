203fc291982ac52ed49df61d707cac44ed15b18a
Pavan Sabnis <pvnsbns@gmail.com>
2018-02-22 11:56:17 -0800

    Encapsulated the sound playing functionality into the method playSound(soundFileName).

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,7 +13,7 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     // Create global instance for audio player
     var audioPlayer: AVAudioPlayer!
-    
+    let soundList = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +22,33 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        var selectedSoundFileName = soundList[sender.tag - 1]
+        
+        playSound(soundFileName: selectedSoundFileName)
+            
+    }
+    
+    
+    
+    func playSound(soundFileName: String) {
         //Although the music buttons have the same IBAction, the tag property differentiates each button
         //This entire code within this function is reponsible for playing sound at button press
         //This is the space where I create & play the url for each of the sound files.
         //bundles are used to locate and use other resources, in this case use a URL to play sound files
-        
-        var soundList = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
-        let soundURL: URL? = Bundle.main.url(forResource: soundList[sender.tag - 1], withExtension: "wav")
+        let soundURL: URL? = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
+            //A try statement is required here just in case an audio file isn't found
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
         }
         catch {
+            print("Failed to access the sound file successfully")
             print(error.localizedDescription)
         }
         
         audioPlayer.play()
-            
     }
+    
         
 }
     
